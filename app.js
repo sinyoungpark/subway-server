f6c9ef38857e232d7f92d5d0838448ec08c361f7
sinyoung98 <joyhope3@naver.com>
2022-10-25 23:10:33 +0900

    edit app.js
    -sequelize 설치, sequelize 모델 생성

--- app.js
+++ app.js
@@ -1,37 +1,68 @@
-/*1. add http module */
+
 const http = require("http");
+const { Sequelize, DataTypes, Model } = require("sequelize");
+
+require("dotenv").config();
+
+/*create a sequelize instance 
+*/
+
+
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.PASSWORD, {
+  host : 'localhost',
+  dialect : 'mysql'
+});
+
+// const User = sequelize.define("User",{
+//   firstname : {
+//     type : DataTypes.STRING,
+//     allowNull : false
+//   },
+//   lastName : {
+//     type : DataTypes.STRING,
+//     //allowNUll defaults to true
+//   }
+// },{
+//   //other model options go here 
+// });
+
+class User extends Model {
+  otherPublicField;
+}
+
+User.init({
+  id : {
+    type : DataTypes.INTEGER,
+
+    autoIncrement : true,
+    primaryKey : true
+  }
+}, { sequelize });
+
+const user = new User({id:1});
+user.id;
+
+console.log(User === sequelize.models.User);
 
 const hostname = '127.0.0.1';
 const port = 5000;
 
-/*createServer =  http method, create a new HTTP server and server를 return.*/
-const server = http.createServer((req, res) => {
 
-  /*요청에 대한 응답 코드로 200을 세팅한다. 
-   200은 성공적 응답을 뜻한다. 
-  */
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  /*
-  content-type header 세팅하기 
-  */
   res.setHeader("content-type", "text/plain");
-  /*close the respone. augument로 content를 넣어보낼 수 있다. */
   res.end("hello world");
 });
 
-/*server port, hostname을 세팅해주어야 함.  
-  server가 준비되면 > callback function 이 실행됨. 
-*/
-server.listen(port, hostname, () => {
+
+server.listen(port, hostname, async () => {
   console.log(`server running at http://${hostname}:${port}/`);
+  /*testing the connection */
+  try{
+    await sequelize.authenticate();
+    console.log('successfullly.')
+  } catch(error){
+    console.error('failed',error);
+  }
 });
 
-/*
-127.0.0.1 로 reqeset가 들어오면 
-request 이벤트가 실행된다. > providing request, response 
-
--request : http.IncomingMessage object
--response : http.ServerResponse object
-
-이 2개의 객체는ㄴ http call을 핸들링하는데 필요한 필수 요소이다. 
-*/
