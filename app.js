fc1efdbe46d090a6504c2502c1f85e6c3421cdb6
sinyoung98 <joyhope3@naver.com>
2022-10-31 19:48:12 +0900

    edit app.js
    - port 번호 .env 파일의 환경변수 PORT

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 
-
 require("dotenv").config();
 const { Sequelize, DataTypes, Model } = require("sequelize");
-
 /*router */
 const customerRouter = require("./routes/customerRouter");
+
+
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.PASSWORD, {
   host : 'localhost',
@@ -14,15 +14,15 @@ const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER,
 });
 
 const app = express();
-const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 
+
 app.use("/customers", customerRouter);
 
-app.listen(port, async () => {
-  console.log(`server running at http://localhost:${port}/`);
+app.listen(process.env.PORT, async () => {
+  console.log(`server running at http://localhost:${process.env.PORT}/`);
   try{
     await sequelize.authenticate();
     console.log('successfullly.')
