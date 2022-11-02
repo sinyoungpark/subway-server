ae44c0a04f25a11ac60f78f96a52932ae1a782dd
sinyoung98 <joyhope3@naver.com>
2022-11-02 20:45:39 +0900

    add sandwich model

---
+++ models/sandwich.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Sandwich extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Sandwich.init({
+    menu: DataTypes.STRING,
+    type: DataTypes.STRING,
+    kcal: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Sandwich',
+  });
+  return Sandwich;
+};
