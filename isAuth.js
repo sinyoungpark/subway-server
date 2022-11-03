dd3956bac419b5e6feefba0e8c002d2cc2c53814
sinyoung98 <joyhope3@naver.com>
2022-11-03 17:19:05 +0900

    add isAuth.js
    - 유저인증 함수
    request header의 authorization 값을 체크.
    accesstoken 값을 디코딩하여 userId 정보 읽기
    userId 값이 있다면 db에 있다면 접근 허용

---
+++ isAuth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const isAuth = (req) => {
+  const authorization = req.get('authorization');
+  if(!authorization) throw new Error('잘못된 접근입니다.');
+
+  const token = authorization.split(" ")[1];
+  const {userId} = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  return userId;
+}
+
+module.exports = isAuth;
