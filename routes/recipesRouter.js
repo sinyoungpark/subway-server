45aff936988f179547bcc1bc36b1248052e3ca98
sinyoung98 <joyhope3@naver.com>
2022-11-04 14:36:14 +0900

    add /recipes 게시글 가져오기 api
    - m:n association
    1. 조인테이블 생성
    2. association 정의
    3. Board findAll할 때 include 옵션에 추가하기

--- routes/recipesRouter.js
+++ routes/recipesRouter.js
@@ -16,9 +16,19 @@ router.post("/", async(req, res) => {
       const post = await db.Board.create({
         userId,
         menuId,
-        ingredientId,
         title,
         likes
+      });
+
+      /*Board-ingredients 테이블 안에 넣기
+
+      1. board-ingredients안에 post.id와 ingredientsId 값을 넣는다. 
+      */
+      ingredientId.forEach(async(ingredient) => {
+        await db.Board_Ingredient.create({
+          boardId : post.id, 
+          ingredientId : ingredient
+        });
       });
 
       res.status(201).send({
@@ -33,9 +43,52 @@ router.post("/", async(req, res) => {
   }
 });
 
-/*get요청*/
+/*유저가 작성한 게시글들 get*/
 router.get("/", async (req, res) => {
+  try{
+    const userId = isAuth(req);
+    if(userId){
+      //board-menu
+      const postsMenu = await db.Board.findAll({
+        where : {
+          userId,
+        },
+        include : [db.Menu, db.Ingredient]
+      });
 
+      const sendData = [];
+      postsMenu.forEach((post) => {
+        const {id, likes, Menu, Ingredients} = post;
+        const ingredientsData = [];
+        const ingredientsImg = [];
+
+        Ingredients.forEach((val) => {
+          ingredientsData.push(val.name);
+          ingredientsImg.push(val.img);
+        });
+
+        sendData.push({
+          id, 
+          likes,
+          menuImg : Menu.img,
+          menu : Menu.name, 
+          ingredients : ingredientsData,
+          ingredientsImg 
+        });
+      });
+
+      console.log(sendData);
+
+      res.send({
+        message : "okay"
+      })
+    }
+  }
+  catch(e){
+    res.send({
+      error : `${e.message}`
+    });
+  }
 });
 
 module.exports = router;
