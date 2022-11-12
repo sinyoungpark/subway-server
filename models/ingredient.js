05372d796aedf568408e1d829a94ed09d7f3f6e8
sinyoung98 <joyhope3@naver.com>
2022-11-13 03:32:36 +0900

    edit type, ingredients models
    - type, ingredients association 정의
    one-to-many 관계
    type.hasMany
    ingredients.belongsTo

--- models/ingredient.js
+++ models/ingredient.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
 
       //board-ingredient
       Ingredient.hasMany(models.Board_Ingredient);
+      Ingredient.belongsTo(models.Type);
     }
   }
   Ingredient.init({
