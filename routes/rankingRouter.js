6ffd117c821c8fd8b35f4ffc8c8d6106f1d7bcfe
sinyoung98 <joyhope3@naver.com>
2022-11-17 17:20:40 +0900

    edit rankingRouter
    - likes 변경
    
    issue
    좋아요 수에 따른 order

--- routes/rankingRouter.js
+++ routes/rankingRouter.js
@@ -1,40 +1,44 @@
 const express = require("express");
 const isAuth = require("../isAuth");
+const { sequelize } = require("../models");
 const db = require("../models");
-require('dotenv').config();
+require("dotenv").config();
 const router = express.Router();
 
-router.get('/', async(req, res) => {
-  try{
+router.get("/", async (req, res) => {
+  try {
     const userId = isAuth(req);
-    if(userId !== null){
+    if (userId !== null) {
       const rankings = await db.Board.findAll({
-        include : [{
-          model : db.User,
-            attributes : ["name", "profileImg"]
-        },{
-          model : db.Menu,
-          attributes : ["name", "img"]
-        },{
-          model : db.Ingredient,
-          attributes : ["name", "img"]
-        }],
-        attributes : ["id", "title", "likes"],
-        order : [
-          ['likes', 'DESC'],
+        include: [
+          {
+            model: db.User,
+            attributes: ["name", "profileImg"],
+          },
+          {
+            model: db.Menu,
+            attributes: ["name", "img"],
+          },
+          {
+            model: db.Ingredient,
+            attributes: ["name", "img"],
+          },
+          {
+            model: db.Like,
+          },
         ],
-        limit : 10
+        attributes : ["title", "id"],
+        limit: 10,
       });
 
       res.status(200).send({
-        data : rankings
+        data: rankings,
       });
     }
-  }
-  catch(error){
+  } catch (error) {
     res.send({
-      message : error
-    })
+      message: error.message,
+    });
   }
 });
 
