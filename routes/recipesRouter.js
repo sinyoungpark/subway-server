40255b0d32c6e95872bd7321f3274fe1303cc3d5
sinyoung98 <joyhope3@naver.com>
2022-11-17 15:40:53 +0900

    edit recipesRouter
    - 좋아요 버튼 라우터 작성
    - /recipes patch 요청, postId가 있을 경우 실행

--- routes/recipesRouter.js
+++ routes/recipesRouter.js
@@ -1,64 +1,63 @@
-const express = require('express');
-const db = require('../models');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const db = require("../models");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
-const isAuth = require('../isAuth');
+const isAuth = require("../isAuth");
 
-require('dotenv').config();
+require("dotenv").config();
 
 /*게시글 작성*/
-router.post("/", async(req, res) => {
-  const {menuId, ingredientId, title, likes} = req.body;
+router.post("/", async (req, res) => {
+  const { menuId, ingredientId, title, likes } = req.body;
 
-  try{
+  try {
     const userId = isAuth(req);
-    if(userId !== null){
+    if (userId !== null) {
       const post = await db.Board.create({
         userId,
         menuId,
         title,
-        likes
+        likes,
       });
 
       /*Board-ingredients 테이블 안에 넣기
 
       1. board-ingredients안에 post.id와 ingredientsId 값을 넣는다. 
       */
-      ingredientId.forEach(async(ingredient) => {
+      ingredientId.forEach(async (ingredient) => {
         await db.Board_Ingredient.create({
-          boardId : post.id, 
-          ingredientId : ingredient
+          boardId: post.id,
+          ingredientId: ingredient,
         });
       });
 
       res.status(201).send({
-        data : '저장되었습니다.'
+        data: "저장되었습니다.",
       });
     }
-  }
-  catch(e){
+  } catch (e) {
     res.send({
-      error : `${e.message}`
+      error: `${e.message}`,
     });
   }
 });
 
 /*유저가 작성한 게시글들 get*/
 router.get("/", async (req, res) => {
-  try{
+  try {
     const userId = isAuth(req);
-    if(userId){
+    if (userId) {
       //board-menu
       const postsMenu = await db.Board.findAll({
-        where : {
+        where: {
           userId,
         },
-        include : [db.Menu, db.Ingredient]
+        include: [db.Menu, db.Ingredient],
       });
 
       const sendData = [];
       postsMenu.forEach((post) => {
-        const {id, likes, Menu, Ingredients} = post;
+        const { id, likes, Menu, Ingredients } = post;
         const ingredientsData = [];
         const ingredientsImg = [];
 
@@ -68,75 +67,91 @@ router.get("/", async (req, res) => {
         });
 
         sendData.push({
-          id, 
+          id,
           likes,
-          menuImg : Menu.img,
-          menu : Menu.name, 
-          ingredients : ingredientsData,
-          ingredientsImg 
+          menuImg: Menu.img,
+          menu: Menu.name,
+          ingredients: ingredientsData,
+          ingredientsImg,
         });
       });
 
-
       res.status(200).send({
-        data : sendData
-      })
+        data: sendData,
+      });
     }
-  }
-  catch(e){
+  } catch (e) {
     res.send({
-      error : `${e.message}`
+      error: `${e.message}`,
     });
   }
 });
 
 /*게시글 수정 */
 router.patch("/", async (req, res) => {
-  const {postId, menuId, ingredientId, title, likes} = req.body;
-  try{
+  const { postId, menuId, ingredientId, title, likes } = req.body;
+  try {
     const userId = isAuth(req);
-    if(userId !== null){
-      const post = await db.Board.findByPk(postId);
-      post.update({
-        menuId,
-        title, 
-        likes
-      });
+    if (userId !== null) {
+      const postId = req.query.postId;
+      //좋아요 요청
+      if (postId) {
+        const userLikes = await db.Like.findOne({
+          where: {
+            boardId: postId,
+            userId,
+          },
+        });
+        //좋아요 삭제
+        if (userLikes) {
+          await db.Like.destroy(userLikes);
+        } else {
+          //Likes에 추가
+          await db.Like.create({
+            boardId: postId,
+            userId,
+          });
+        }
+      } else {
+        const post = await db.Board.findByPk(postId);
+        post.update({
+          menuId,
+          title,
+          likes,
+        });
+      }
 
       res.status(200).send({
-        data : '성공적으로 수정 되었습니다.'
+        data: "성공적으로 수정 되었습니다.",
       });
     }
-  }
-  catch(e){
+  } catch (e) {
     res.send({
-      error : `${e.meesage}`
-    })
+      error: `${e.message}`,
+    });
   }
 });
 
 /*게시글 삭제 */
-router.delete("/", async(req,res) => {
-  const {postId} = req.query;
-  try{
+router.delete("/", async (req, res) => {
+  const { postId } = req.query;
+  try {
     const userId = isAuth(req);
-    if(userId !== null){
-
+    if (userId !== null) {
       const post = await db.Board.findOne({
-        where : { id : postId }
+        where: { id: postId },
       });
       await post.destroy();
 
       res.status(200).send({
-        message : "삭제되었습니다."
+        message: "삭제되었습니다.",
       });
     }
-  }
-  catch(e){
+  } catch (e) {
     res.send({
-      error : `${req.params}`
-    })
+      error: `${req.params}`,
+    });
   }
-})
+});
 
 module.exports = router;
